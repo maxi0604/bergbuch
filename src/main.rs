cafbb39e7deeb741c8a2f7b8a85f43f8a5220837
Maximilian R. <public@complexvector.space>
2024-03-08 21:55:22 +0100

    Return exit code

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ use std::{
     env::args_os,
     fs,
     io::{stdin, stdout, IsTerminal, Write},
-    path::Path,
+    path::Path, process::ExitCode,
 };
 
 use bergbuch::interpreter::Interpreter;
 
-fn main() {
+fn main() -> ExitCode {
+    if args_os().len() > 2 {
+        eprintln!("usage: lox [file]");
+        return ExitCode::FAILURE;
+    }
+
     if let Some(arg) = args_os().nth(1) {
         run_file(Path::new(&arg));
     } else {
         run_prompt();
     }
+
+    ExitCode::SUCCESS
 }
 
 fn run_file(path: &Path) {
