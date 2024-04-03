c49dd5b6f4deb190fbac191c46429c6b6c40ecd4
Maximilian R <public@complexvector.space>
2024-04-03 04:51:15 +0200

    Start writing vm

--- src/vm/chunk.rs
+++ src/vm/chunk.rs
@@ -1,12 +1,15 @@
 use std::fmt::Display;
 
+use num_derive::FromPrimitive;
+
 #[repr(u8)]
+#[derive(FromPrimitive, Debug)]
 pub enum OpCode {
     Const = 0x0,
     Return
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     Num(f64)
 }
@@ -32,5 +35,17 @@ impl Chunk {
     pub fn add_const(&mut self, val: Value) -> usize {
         self.consts.push(val);
         self.consts.len() - 1
+    }
+
+    pub fn read_byte(&self, idx: usize) -> u8 {
+        self.code[idx]
+    }
+
+    pub fn get_line(&self, idx: usize) -> usize {
+        self.lines[idx]
+    }
+
+    pub fn get_const(&self, idx: usize) -> Value {
+        self.consts[idx].clone()
     }
 }
